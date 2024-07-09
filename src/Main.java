12ca1f0dc71f529557d7c9c4e58fce4ecadcd4d4
kr-devesh-0246 <devesh2462003@gmail.com>
2024-07-09 09:14:54 +0530

    added driver code

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BinaryTree tree = new BinaryTree();
+        tree.populate(sc);
+//        tree.display();
+        tree.prettyDisplay();
+    }
+
 }

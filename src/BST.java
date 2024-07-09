d98e641613bacdf8379579985620ba83ff58c87a
kr-devesh-0246 <devesh2462003@gmail.com>
2024-07-09 11:22:36 +0530

    added traversal implementations

--- src/BST.java
+++ src/BST.java
@@ -40,4 +40,54 @@ public class BST {
         }
         System.out.println(details + node.getValue());
     }
+
+    public Node insert(int value, Node node) {
+        if (node == null) {
+            node = new Node(value);
+            return node;
+        }
+
+        if (value < node.value)
+            node.left = insert(value, node.left);
+
+        if (value > node.value)
+            node.right = insert(value, node.right);
+
+        node.height = Math.max(height(node.left), height(node.right)) + 1;
+
+        return node;
+    }
+
+    public void preOrder() {
+        preOrder(root);
+    }
+    private void preOrder(Node node) {
+        if (node == null)
+            return;
+        System.out.println(node.value + " ");
+        preOrder(node.left);
+        preOrder(node.right);
+    }
+
+    public void postOrder() {
+        postOrder(root);
+    }
+    private void postOrder(Node node) {
+        if (node == null)
+            return;
+        postOrder(node.left);
+        postOrder(node.right);
+        System.out.println(node.value + " ");
+    }
+
+    public void inOrder() {
+        inOrder(root);
+    }
+    private void inOrder(Node node) {
+        if (node == null)
+            return;
+        inOrder(node.left);
+        System.out.println(node.value + " ");
+        inOrder(node.right);
+    }
 }
